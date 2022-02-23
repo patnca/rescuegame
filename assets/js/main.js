14b365c3682341c3065247d4c6f044ea7e3cd0d7
Patrick França <psfranca@gmail.com>
2022-02-22 22:22:35 -0300

    inicializando projeto do game Resgate

---
+++ assets/js/main.js
@@ -0,0 +1,62 @@
+
+function start() {
+    $(".inicio").hide();
+
+    $(".fundoGame").append("<div id='jogador' class='anima1'></div>");
+    $(".fundoGame").append("<div id='inimigo1' class='anima2'></div>");
+    $(".fundoGame").append("<div id='inimigo2'></div>");
+    $(".fundoGame").append("<div id='amigo' class='anima3'></div>");
+    
+
+    var jogo = {}
+    var TECLA = {
+        W: 87,
+        S: 83,
+        D: 68
+    }
+
+    jogo.pressionou = [];
+
+    $(document).keydown(function(e){
+        jogo.pressionou[e.which] = true;
+    });
+
+    $(document).keyup(function(e){
+        jogo.pressionou[e.which] = false;
+    });
+
+    jogo.timer = setInterval(loop,30);
+
+    function loop() {
+        
+        moveFundo();
+        moveJogador();
+    }
+
+    function moveFundo() {
+        esquerda = parseInt($(".fundoGame").css("background-position"));
+        $(".fundogame").css("background-position", esquerda -1);
+    }
+
+    function moveJogador() {
+        if(jogo.pressionou[TECLA.W]) {
+            var topo = parseInt($("#jogador").css("top"));
+            $("#jogador").css("top",topo - 10);
+
+            if(topo <= 10) {
+                $("#jogador").css("top",topo + 10);
+            }
+        }
+        if(jogo.pressionou[TECLA.S]) {
+            var topo = parseInt($("#jogador").css("top"));
+            $("#jogador").css("top",topo + 10);
+
+            if(topo >= 434) {
+                $("#jogador").css("top",topo - 10);
+            }
+        }
+        if(jogo.pressionou[TECLA.D]) {
+            
+        }
+    }
+}
